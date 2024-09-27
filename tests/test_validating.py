d50095a806f79d57fc8cfdb63f165f9a8f98316c
Mark Van de Streek <m.van.de.streek@st.hanze.nl>
2024-09-27 11:40:50 +0200

    Setup a test environment to run tests using GitHub actions

---
+++ tests/test_validating.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+To be filed in later...
+"""
+
+__author__ = "Mark Van de Streek"
+__data__ = "2024-09-24"
+
+import sys
+import os
+import subprocess
+import pytest
+
+
+def test_example():
+    assert 1 + 1 == 2
