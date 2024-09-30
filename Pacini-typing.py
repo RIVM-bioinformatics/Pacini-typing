34286f13ed3b6951b50c3a6d1b30c0e386bb9fbc
Mark Van de Streek <m.van.de.streek@st.hanze.nl>
2024-09-30 14:39:02 +0200

    Replaced unnecessary LOGGER variable use in the script

--- Pacini-typing.py
+++ Pacini-typing.py
@@ -18,6 +18,7 @@ import logging
 import makedatabase
 import argument_parser.build_parser
 import validating.validating_input_arguments
+import validating.validating_input_file
 
 LOGGER = logging.getLogger(__name__)
 
@@ -32,8 +33,18 @@ if __name__ == "__main__":
         level=args.verbose and logging.DEBUG or logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s")
 
-    validating.validating_input_arguments.main(args)
+    logging.info("Starting the program...")
+    logging.debug(f"Arguments have been parsed, validating them...")
+
+    if validating.validating_input_arguments.main(args):
+        logging.debug("Input arguments have been validated")
 
     # Check if the options are available
     if args.options == "makedatabase":
+        logging.info("Running makedatabase")
         makedatabase.main(args)
+    elif args.options == "query":
+        if hasattr(args, "single") and args.single:
+            validating.validating_input_file.main([args.single])
+        elif hasattr(args, "paired") and args.paired:
+            validating.validating_input_file.main(args.paired)
