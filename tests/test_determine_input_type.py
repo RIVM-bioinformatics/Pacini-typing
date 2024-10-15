68c4d0958672c9639817ba923d6dcc5a3c085ac9
Mark van de Streek <streevdm@rivm-biohn-l06p.int.ssc-campus.nl>
2024-10-15 16:17:38 +0200

    fix: tried to fix a github actions problem

--- tests/test_determine_input_type.py
+++ tests/test_determine_input_type.py
@@ -14,44 +14,35 @@ __author__ = "Mark Van de Streek"
 __data__ = "2024-09-24"
 __all__ = ["test_get_file_type"]
 
-import sys
-
 from validating.determine_input_type import FileValidator
 
-# FILE_VALIDATOR = FileValidator(["test_data/VIB_AA2784AA_AS.scaffold.fasta"])
-# PAIRED_VALIDATOR = FileValidator(
-#     ["test_data/ERR976461_1.fastq", "test_data/ERR976461_2.fastq"]
-# )
+FILE_VALIDATOR = FileValidator(["./test_data/VIB_AA2784AA_AS.scaffold.fasta"])
+PAIRED_VALIDATOR = FileValidator(
+    ["./test_data/ERR976461_1.fastq", 
+     "./test_data/ERR976461_2.fastq"])
 
 
-# def test_get_file_type():
-#     """
-#     Test the retrieve_body() function from the determine_input_type.py module
-#     The function retrieves the body of the input file
+def test_get_file_type():
+    """
+    Test the retrieve_body() function from the determine_input_type.py module
+    The function retrieves the body of the input file
 
-#     Self.body is a dictionary with the input files as keys and the body of the file as values:
-#     {
-#         file1: [line1, line2, line3, line4, line5],
-#         file2: [line1, line2, line3, line4, line5]
-#     }
-#     """
-#     assert FILE_VALIDATOR.get_file_type() == "FASTA"
-#     assert PAIRED_VALIDATOR.get_file_type() == "FASTQ"
-
-
-# def test_body():
-#     """
-#     Test function that test the main goal of the FileValidator class,
-#     determine the file type.
-#     It passes the file in test_data/ as input and makes sure the output is correct
-#     """
-#     assert FILE_VALIDATOR.body["test_data/VIB_AA2784AA_AS.scaffold.fasta"][
-#         0
-#     ].startswith(">")
+    Self.body is a dictionary with the input files as keys and the body of the file as values:
+    {
+        file1: [line1, line2, line3, line4, line5],
+        file2: [line1, line2, line3, line4, line5]
+    }
+    """
+    assert FILE_VALIDATOR.get_file_type() == "FASTA"
+    assert PAIRED_VALIDATOR.get_file_type() == "FASTQ"
 
 
-# def end2end():
-#     """
-#     Still to be implemented...
-#     """
-#     myList = ["A", "B", "C"]
+def test_body():
+    """
+    Test function that test the main goal of the FileValidator class,
+    determine the file type.
+    It passes the file in test_data/ as input and makes sure the output is correct
+    """
+    assert FILE_VALIDATOR.body["./test_data/VIB_AA2784AA_AS.scaffold.fasta"][
+        0
+    ].startswith(">")
