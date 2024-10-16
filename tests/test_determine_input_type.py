b9a706f39bb2c37a1669c81e26597cf0fd52770c
Mark Van de Streek <m.van.de.streek@st.hanze.nl>
2024-10-16 10:16:00 +0200

    test: github action workflow path test

--- tests/test_determine_input_type.py
+++ tests/test_determine_input_type.py
@@ -16,34 +16,34 @@ __all__ = ["test_get_file_type"]
 
 from validating.determine_input_type import FileValidator
 
-# FILE_VALIDATOR = FileValidator(["./../test_data/VIB_AA2784AA_AS.scaffold.fasta"])
+FILE_VALIDATOR = FileValidator(["test_data/VIB_AA2784AA_AS.scaffold.fasta"])
 # PAIRED_VALIDATOR = FileValidator(
 #     ["./../test_data/ERR976461_1.fastq", "./../test_data/ERR976461_2.fastq"]
 # )
 
 
-# def test_get_file_type():
-#     """
-#     Test the retrieve_body() function from the determine_input_type.py module
-#     The function retrieves the body of the input file
-
-#     Self.body is a dictionary with the input files as keys and the body of the file as values:
-#     {
-#         file1: [line1, line2, line3, line4, line5],
-#         file2: [line1, line2, line3, line4, line5]
-#     }
-#     """
-#     assert FILE_VALIDATOR.get_file_type() == "FASTA"
-#     assert PAIRED_VALIDATOR.get_file_type() == "FASTQ"
-
-
-def test_body():
+def test_get_file_type():
     """
-    Test function that test the main goal of the FileValidator class,
-    determine the file type.
-    It passes the file in test_data/ as input and makes sure the output is correct
+    Test the retrieve_body() function from the determine_input_type.py module
+    The function retrieves the body of the input file
+
+    Self.body is a dictionary with the input files as keys and the body of the file as values:
+    {
+        file1: [line1, line2, line3, line4, line5],
+        file2: [line1, line2, line3, line4, line5]
+    }
     """
-    print("hello!")
-    # assert FILE_VALIDATOR.body["./../test_data/VIB_AA2784AA_AS.scaffold.fasta"][
-    #     0
-    # ].startswith(">")
+    assert FILE_VALIDATOR.get_file_type() == "FASTA"
+    # assert PAIRED_VALIDATOR.get_file_type() == "FASTQ"
+
+
+# def test_body():
+#     """
+#     Test function that test the main goal of the FileValidator class,
+#     determine the file type.
+#     It passes the file in test_data/ as input and makes sure the output is correct
+#     """
+#     print("hello!")
+#     # assert FILE_VALIDATOR.body["./../test_data/VIB_AA2784AA_AS.scaffold.fasta"][
+#     #     0
+#     # ].startswith(">")
