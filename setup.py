d4af1078d15d9e231087270ab4ab7d2645d5b227
MarkStreek <mvdstreek2003@gmail.com>
2026-02-26 13:46:40 +0100

    refactor: remove dynamic version retrieval and set version to 3.0.0

--- setup.py
+++ setup.py
@@ -13,36 +13,12 @@ It is used when cloning the repository and installing through Bioconda.
 
 __author__ = "Mark van de Streek"
 __date__ = "2024-11-06"
-__all__ = ["get_version"]
 
 from setuptools import find_packages, setup
 
-from command_utils import CommandInvoker, ShellCommand
-
-
-def get_version() -> str:
-    """
-    Get the version of the package from git tags.
-    If there are no tags, return "0.0.0".
-    ----------
-    Output:
-        - version: version of the package
-    ----------
-    """
-    try:
-        result = CommandInvoker(
-            ShellCommand(["git", "describe", "--tags"], capture=True)
-        ).execute()
-        if isinstance(result, tuple):
-            return result[0].strip().split("-")[0]
-    except Exception:
-        return "0.0.0"
-    return "0.0.0"
-
-
 setup(
     name="pacini_typing",
-    version=get_version(),
+    version="3.0.0",
     author="Mark van de Streek",
     author_email="ids-bioinformatics@rivm.nl",
     packages=find_packages(),
